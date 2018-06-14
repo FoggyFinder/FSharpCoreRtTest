17e4cd2b2a6590c835f07c376395edadc417f1eb
FoggyFinder <FoggyFinder@yandex.ua>
2018-06-14 13:01:14 +0300

    move to separate library

---
+++ FLibrary/ConsoleApp/Program.fs
@@ -0,0 +1,7 @@
+﻿open System
+open FLibrary
+[<EntryPoint>]
+let main argv =
+    Say.helloWorld()
+    // Say.hello "HelloWorld"
+    0 // return an integer exit code
---
+++ FLibrary/FLibrary/Library.fs
@@ -0,0 +1,7 @@
+namespace FLibrary
+
+module Say =
+    let helloWorld() = 
+        printfn "Hello World" 
+    let hello name =
+        printfn "Hello %s" name
