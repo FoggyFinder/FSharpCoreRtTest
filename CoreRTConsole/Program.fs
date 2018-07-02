c6682ab59c63bd190bd8f9ab0a39d5b9b62e2187
Charles Roddie <charles@roddie.name>
2018-07-02 17:43:37 +0100

    tail call tests

--- CoreRTConsole/Program.fs
+++ CoreRTConsole/Program.fs
@@ -2,24 +2,9 @@
 open FSLibrary
 open CSLibrary
 
-let prTest() = 
-    let str = FSLibrary.testString()
-    Console.WriteLine (str)
-    printfn "%s" str   
-    
-let eTest() = 
-    let c = C()
-    let h1 = ChannelChangedHandler(fun _ ch -> Console.WriteLine("Channel = {0}", ch))
-    c.ChannelChanged.AddHandler(h1)
-    c.ChangeChannel(3)
-
-let expTest() = 
-    [| 0..5 |].Map(fun x -> x * 2)
-    |> Seq.iter(fun v -> Console.WriteLine(v))
-
 [<EntryPoint>]
 let main argv =
-    prTest()
+    Console.WriteLine(testString)
     // eTest()
     // expTest()
     Console.ReadLine() |> ignore
--- FSLibrary/Library.fs
+++ FSLibrary/Library.fs
@@ -15,19 +15,19 @@ let s = System.Text.StringBuilder()
 //    :?> System.Linq.Expressions.Expression<int>
 
 // NewtonSoft.Json works on UWP. On CoreRT it requires rd.xml.
-module TestJsonSerialiation =
-    type Tree =
-        | Branch of Tree * Tree
-        | Leaf of int
-    let tree = Branch(Branch(Leaf 3, Leaf 4),Leaf 4)
-    type IdTree =
-        { Id:int
-          Tree: Tree }
-    let idTree = {Id = 3; Tree = tree}
-    let serializedIdTree = JsonConvert.SerializeObject(idTree)
-    let serializedIdTree' = serializedIdTree |> JsonConvert.DeserializeObject<IdTree> |> JsonConvert.SerializeObject
-    s.AppendLine(serializedIdTree) |> ignore
-    Assert.AreEqual(serializedIdTree',serializedIdTree)
+//module TestJsonSerialiation =
+//    type Tree =
+//        | Branch of Tree * Tree
+//        | Leaf of int
+//    let tree = Branch(Branch(Leaf 3, Leaf 4),Leaf 4)
+//    type IdTree =
+//        { Id:int
+//          Tree: Tree }
+//    let idTree = {Id = 3; Tree = tree}
+//    let serializedIdTree = JsonConvert.SerializeObject(idTree)
+//    let serializedIdTree' = serializedIdTree |> JsonConvert.DeserializeObject<IdTree> |> JsonConvert.SerializeObject
+//    s.AppendLine(serializedIdTree) |> ignore
+//    Assert.AreEqual(serializedIdTree',serializedIdTree)
 
 module TailRecursion =
     // from https://gist.github.com/dsyme/82c49d5cb63d04f3b2b3502d51e6277c
@@ -35,7 +35,9 @@ module TailRecursion =
     
     // Mutually recurisve tail call, compiler will emit tail instruction
     // does not work on UWP when HugeInt = 10000000
-    // works when HugeInt = 100
+    // works on CoreRT when HugeInt = 10000000
+    // works on UWP and CoreRT when HugeInt = 100
+    // Conclusion: UWP ignores .tail while CoreRT implements it.
     let rec mutualTail1IsOdd x = 
         match x with
         | 1 -> true
@@ -50,11 +52,11 @@ module TailRecursion =
     s.AppendLine("HugeInt is odd: " + odd.ToString()) |> ignore
     s.AppendLine("HugeInt is even: " + even.ToString()) |> ignore
 
-type ChannelChangedHandler = delegate of obj * int -> unit
-type C() =  
-    let channelChanged = new Event<ChannelChangedHandler,_>()
-    [<CLIEvent>]    
-    member self.ChannelChanged = channelChanged.Publish
-    member self.ChangeChannel(n) = channelChanged.Trigger(self,n)
+//type ChannelChangedHandler = delegate of obj * int -> unit
+//type C() =  
+//    let channelChanged = new Event<ChannelChangedHandler,_>()
+//    [<CLIEvent>]    
+//    member self.ChannelChanged = channelChanged.Publish
+//    member self.ChangeChannel(n) = channelChanged.Trigger(self,n)
 
-let testString() = s.ToString()
+let testString = s.ToString()
