5514dee58bce180c53ebb0878cf70723daf1647d
Charles Roddie <charles@roddie.name>
2018-07-02 15:12:29 +0100

    move failing test to MainIssues branch

--- FSLibrary/Library.fs
+++ FSLibrary/Library.fs
@@ -2,14 +2,6 @@ module FSLibrary
 
 open LibraryTestFx
 open System
+
 // Define a string testString for display.
-
 let testString() = "Hello World"
-
-type ChannelChangedHandler = delegate of obj * int -> unit
-type C() =  
-    let channelChanged = new Event<ChannelChangedHandler,_>()
-
-    [<CLIEvent>]    
-    member self.ChannelChanged = channelChanged.Publish
-    member self.ChangeChannel(n) = channelChanged.Trigger(self,n)
