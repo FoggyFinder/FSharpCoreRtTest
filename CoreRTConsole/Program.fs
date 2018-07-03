fd7a5a61c642ebec21b9ff2fbfffc02e7774ae6a
Charles Roddie <charles@roddie.name>
2018-07-03 04:59:18 +0100

    clean up master branch

--- CoreRTConsole/Program.fs
+++ CoreRTConsole/Program.fs
@@ -1,26 +1,8 @@
 ﻿open System
 open FSLibrary
-open CSLibrary
-
-let prTest() = 
-    let str = FSLibrary.testString()
-    Console.WriteLine (str)
-    printfn "%s" str   
-    
-let eTest() = 
-    let c = C()
-    let h1 = ChannelChangedHandler(fun _ ch -> Console.WriteLine("Channel = {0}", ch))
-    c.ChannelChanged.AddHandler(h1)
-    c.ChangeChannel(3)
-
-let expTest() = 
-    [| 0..5 |].Map(fun x -> x * 2)
-    |> Seq.iter(fun v -> Console.WriteLine(v))
 
 [<EntryPoint>]
 let main argv =
-    prTest()
-    // eTest()
-    // expTest()
+    Console.WriteLine testString
     Console.ReadLine() |> ignore
     0 // return an integer exit code
--- FSLibrary/Library.fs
+++ FSLibrary/Library.fs
@@ -4,4 +4,4 @@ open LibraryTestFx
 open System
 
 // Define a string testString for display.
-let testString() = "Hello World"
+let testString = "Hello World"
